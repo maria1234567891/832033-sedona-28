5ae67033406f4b8ba1d20534f0736d1f8b0265ca
mariamarevskaia <marmar89@mail.ru>
2020-06-25 18:46:14 +0300

    Sedona 1

---
+++ js/popup.js
@@ -0,0 +1,50 @@
+var link = document.querySelector(".search");
+var popup = document.querySelector(".popup");
+var form = document.querySelector(".form");
+var arrival_date = document.querySelector(".arrival_date_input");
+var departure_date = document.querySelector(".departure_date_input");
+var adults_input = document.querySelector(".adults_input");
+var children_input = document.querySelector(".children_input");
+var isStorageSupport = true;
+var storage_adults = "";
+var storage_children = "";
+
+try {
+  storage_adults = localStorage.getItem("adults");
+  storage_children = localStorage.getItem("children");
+  adults_input.value = storage_adults;
+  children_input.value = storage_children;
+} catch (err) {
+  isStorageSupport = false;
+}
+link.addEventListener("click", function (event){
+    event.preventDefault();
+    if(popup.classList.contains("hidden")){
+      popup.classList.remove("hidden");
+    if (storage_adults) {
+      adults_input.value = storage_adults;
+      children_input.value = storage_children;
+    }
+  }
+  else{
+    popup.classList.add("hidden");
+  }
+});
+form.addEventListener("submit", function(evt){
+  if( !arrival_date.value || !departure_date.value || !adults_input.value || !children_input.value){
+    evt.preventDefault();
+    console.log("Нужно ввести даты поездки и количество взрослых и детей");
+  }
+  else{
+    localStorage.setItem("adults", adults_input.value);
+    localStorage.setItem("children", children_input.value);
+  }
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (!popup.classList.contains("hidden")) {
+      evt.preventDefault();
+      popup.classList.add("hidden");
+    }
+  }
+});
